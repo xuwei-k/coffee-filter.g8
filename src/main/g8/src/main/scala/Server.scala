2121642a7415cc9fbf667d835792cce800185d66
kenji yoshida <6b656e6a69@gmail.com>
2017-07-09 07:51:55 +0900

    update url

--- src/main/g8/src/main/scala/Server.scala
+++ src/main/g8/src/main/scala/Server.scala
@@ -9,7 +9,7 @@ object Server {
       case Path("/") => Page("iced venti soy")(Nil)(Nil)(
         <h1>
         <div>How do you like your <a href="http://coffeescript.org/">coffee</a>?</div>
-        <div>Filtered or <a href="http://unfiltered.databinder.net/Unfiltered.html">unfiltered</a>?</div>
+        <div>Filtered or <a href="http://unfiltered.ws">unfiltered</a>?</div>
         </h1>)
     }).run
   }
